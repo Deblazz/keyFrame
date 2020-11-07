371b47dfd8a1c79f41b3432c0e5c82014e93fa12
LorenzoDeBlasio <gioco654@gmail.com>
2020-11-07 20:26:29 +0100

    Arduino reads from sd and keys out

--- src/serialLib/arduino-serial-master/arduinoserial.py
+++ src/serialLib/arduino-serial-master/arduinoserial.py
@@ -1,14 +1,7 @@
-#from __future__ import print_function
 import termios
 import os
-#import sys
-#import time
-#import getopt
 import base64
-
 BPS_SYMS={115200: termios.B115200}
-
-# Indices into the termios tuple.
 IFLAG = 0
 OFLAG = 1
 CFLAG = 2
@@ -16,66 +9,40 @@ LFLAG = 3
 ISPEED = 4
 OSPEED = 5
 CC = 6
-
 def bps_to_termios_sym(bps):
     return BPS_SYMS[bps]
-
 class SerialPort(object):
-    
     def __init__(self, serialport, bps):
-    
         self.fd = os.open(serialport, os.O_RDWR | os.O_NOCTTY | os.O_NDELAY)
         attrs = termios.tcgetattr(self.fd)
-
-        # bps_sym = bps_to_termios_sym(bps)
         bps_sym = BPS_SYMS[bps]
-
-        # Set I/O speed.
         attrs[ISPEED] = bps_sym
         attrs[OSPEED] = bps_sym
-
-        # 8N1
         attrs[CFLAG] &= ~termios.PARENB
         attrs[CFLAG] &= ~termios.CSTOPB
         attrs[CFLAG] &= ~termios.CSIZE
         attrs[CFLAG] |= termios.CS8
-        # No flow control
         attrs[CFLAG] &= ~termios.CRTSCTS
-
-        # Turn on READ & ignore contrll lines.
         attrs[CFLAG] |= termios.CREAD | termios.CLOCAL
-        # Turn off software flow control.
         attrs[IFLAG] &= ~(termios.IXON | termios.IXOFF | termios.IXANY)
-
-        # Make raw.
         attrs[LFLAG] &= ~(termios.ICANON | termios.ECHO | termios.ECHOE | termios.ISIG)
         attrs[OFLAG] &= ~termios.OPOST
-
-        # It's complicated--See
-        # http://unixwiz.net/techtips/termios-vmin-vtime.html
         attrs[CC][termios.VMIN] = 0
         attrs[CC][termios.VTIME] = 20
         termios.tcsetattr(self.fd, termios.TCSANOW, attrs)
-
     def write(self, str):
         os.write(self.fd, str)
-
     def write_byte(self, byte):
         os.write(self.fd, chr(byte))
-
 def main():
     port = "/dev/ttyACM0"
     bps = 115200
     port = SerialPort(port, bps)
-
     # with open("../../../img.jpeg", "rb") as f:
     #     send = base64.b64encode(f.read())
-
     with open("../../../helloworld.txt", "rb") as f:
         send = base64.b64encode(f.read())
-
     #print(send.decode('utf-8'))
     port.write(send)
-
 if __name__ == "__main__":
     main()
